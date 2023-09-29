97d0f9e3751e1f27725578eab30543c5fc524703
kozistr <kozistr@gmail.com>
2023-09-29 13:00:23 +0900

    update: reshape vectors

--- src/endpoints/embedding.rs
+++ src/endpoints/embedding.rs
@@ -2,9 +2,10 @@ use std::collections::HashMap;
 
 use async_once::AsyncOnce;
 use lazy_static::lazy_static;
-use ndarray::{Array, Array1};
+use ndarray::{Array, Array1, Array2};
 use triton_client::inference::model_infer_request::{InferInputTensor, InferRequestedOutputTensor};
 use triton_client::inference::{ModelInferRequest, ModelInferResponse};
+use crate::constants::V1_EMBEDDING_SIZE;
 
 lazy_static! {
     pub static ref CLIENT: AsyncOnce<triton_client::Client> = {
@@ -16,10 +17,26 @@ lazy_static! {
     };
 }
 
+fn serialize_to_byte_string(queries: Vec<String>) -> Vec<u8> {
+    queries
+        .iter()
+        .flat_map(|query: &String| {
+            let len_bytes: Vec<u8> = (query.len() as u32).to_le_bytes().to_vec();
+            len_bytes
+                .into_iter()
+                .chain(query.as_bytes().to_vec().iter().copied())
+        })
+        .collect::<Vec<u8>>()
+}
+
 async fn inference(queries: Vec<String>) -> ModelInferResponse {
+    let shape: Vec<i64> = vec![queries.len() as i64, 1];
+
+    let serialized_queries: Vec<Vec<u8>> = vec![serialize_to_byte_string(queries)];
+
     let inputs: Vec<InferInputTensor> = vec![InferInputTensor {
         name: "text".into(),
-        shape: vec![queries.len() as i64, 1],
+        shape,
         parameters: HashMap::new(),
         datatype: "BYTES".into(),
         contents: None,
@@ -35,24 +52,24 @@ async fn inference(queries: Vec<String>) -> ModelInferResponse {
             name: "embedding".into(),
             parameters: HashMap::new(),
         }],
-        raw_input_contents: queries
-            .into_iter()
-            .map(|query: String| query.as_bytes().to_owned())
-            .collect(),
+        raw_input_contents: serialized_queries,
     };
 
     CLIENT.get().await.model_infer(request).await.unwrap()
 }
 
-pub async fn get_embedding(queries: Vec<String>) -> Vec<Array1<f32>> {
+pub async fn get_embedding(queries: Vec<String>) -> Array2<f32> {
+    let batch_size: usize = queries.len();
     let response: ModelInferResponse = inference(queries).await;
 
-    response
+    let flattend_vectors = response
         .raw_output_contents
         .iter()
         .map(|r: &Vec<u8>| {
             let e: &[f32] = bytemuck::cast_slice::<u8, f32>(r.as_slice());
             Array::from_vec(e.to_vec())
         })
-        .collect::<Vec<Array1<f32>>>()
+        .collect::<Vec<Array1<f32>>>();
+
+    flattend_vectors[0].clone().into_shape((batch_size, V1_EMBEDDING_SIZE)).unwrap()
 }
