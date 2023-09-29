d0086594aa84a5e86723dd5db74290886c2af1be
kozistr <kozistr@gmail.com>
2023-09-29 22:56:34 +0900

    fix: TRITON_SERVER_URL

--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -1,4 +1,4 @@
 // todo : move to docker secret or env wherever
 pub const SERVER_PORT: u16 = 8080;
-pub const TRITON_SERVER_URL: &str = "http://127.0.0.1:8080";
+pub const TRITON_SERVER_URL: &str = "http://127.0.0.1:8001";
 pub const V1_EMBEDDING_SIZE: usize = 2048;
