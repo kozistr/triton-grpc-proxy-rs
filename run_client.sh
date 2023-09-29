cfd073b47bcb21b4da2672cea9e3a809efd7ff05
kozistr <kozistr@gmail.com>
2023-09-29 13:33:28 +0900

    update: example

--- run_client.sh
+++ run_client.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -H "Content-type:application/json" -X POST http://127.0.0.1:8080/v1/embedding -d "[{\"query\": \"asdf\"}, {\"query\": \"asdf asdf\"}]"
+curl -H "Content-type:application/json" -X POST http://127.0.0.1:8080/v1/embedding -d "[{\"query\": \"asdf\"}, {\"query\": \"asdf asdf\"}, {\"query\": \"asdf asdf asdf\"}, {\"query\": \"asdf asdf asdf asdf\"}]"
