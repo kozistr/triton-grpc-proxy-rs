c2325122f4f568c5561477221ad0bd82745a7d98
kozistr <kozistr@gmail.com>
2023-10-09 02:00:06 +0900

    update: TRITON_SERVER_URL

--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -1,6 +1,6 @@
 // todo : move to docker secret or env wherever
 pub const SERVER_PORT: u16 = 8080;
-pub const TRITON_SERVER_URL: &str = "http://127.0.0.1:8001";
+pub const TRITON_SERVER_URL: &str = "http://triton-server:8001";
 pub const MODEL_VERSION: &str = "1";
 pub const MODEL_NAME: &str = "model";
 pub const INPUT_NAME: &str = "text";
