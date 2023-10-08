b21fcef50678a767a4a56f334b69dfbe8d1d9f28
kozistr <kozistr@gmail.com>
2023-10-08 15:06:35 +0900

    feature: ntex

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 #[global_allocator]
-static GLOBAL: MiMalloc = MiMalloc;
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
-use actix_web::{web, App, HttpResponse, HttpServer};
-use mimalloc::MiMalloc;
+use ntex::web::{get, App, HttpResponse, HttpServer, Responder};
 
 use crate::constants::SERVER_PORT;
 use crate::endpoints::get_v1_embedding;
@@ -11,23 +10,18 @@ mod constants;
 mod endpoints;
 mod models;
 
-async fn get_health_status() -> HttpResponse {
-    HttpResponse::Ok()
-        .content_type("application/json")
-        .body("Ok")
+#[get("/health")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().body("ok")
 }
 
-#[actix_web::main]
+#[ntex::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "error");
     env_logger::init();
 
-    HttpServer::new(|| {
-        App::new()
-            .route("/health", web::get().to(get_health_status))
-            .service(get_v1_embedding)
-    })
-    .bind(("127.0.0.1", SERVER_PORT))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(health_check).service(get_v1_embedding))
+        .bind(("127.0.0.1", SERVER_PORT))?
+        .run()
+        .await
 }
