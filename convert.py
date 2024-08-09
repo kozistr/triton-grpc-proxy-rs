62de15f003949b4badd4d6946f7318df8189ee7f
kozistr <kozistr@gmail.com>
2024-08-09 17:02:26 +0900

    update: bge-m3

--- convert.py
+++ convert.py
@@ -13,14 +13,9 @@ class EmbeddingModel(nn.Module):
 
         self.backbone = AutoModel.from_pretrained(model_name)
 
-    def forward(
-        self,
-        input_ids: torch.Tensor,
-        attention_mask: torch.Tensor,
-        token_type_ids: torch.Tensor,
-    ) -> torch.Tensor:
+    def forward(self, input_ids: torch.Tensor, attention_mask: torch.Tensor) -> torch.Tensor:
         return normalize(
-            self.backbone(input_ids, attention_mask, token_type_ids, return_dict=True).last_hidden_state[:, 0], 
+            self.backbone(input_ids, attention_mask, return_dict=True).last_hidden_state[:, 0], 
             p=2.0, 
             dim=1,
         )
@@ -33,33 +28,34 @@ def main():
     torch.onnx.export(
         model,
         (
-            torch.zeros((1, 512), dtype=torch.long),
-            torch.zeros((1, 512), dtype=torch.long),
-            torch.zeros((1, 512), dtype=torch.long),
+            torch.zeros((1, 8192), dtype=torch.long),
+            torch.zeros((1, 8192), dtype=torch.long),
         ),
-        './model_repository/embedding/1/v1.onnx',
+        './model_repository/embedding/1/model.onnx',
         export_params=True,
-        opset_version=16,
+        opset_version=17,
         do_constant_folding=True,
-        input_names=['input_ids', 'attention_mask', 'token_type_ids'],
+        input_names=['input_ids', 'attention_mask'],
         output_names=['embedding'],
         dynamic_axes={
             'input_ids': {0: 'batch_size', 1: 'sequence'},
             'attention_mask': {0: 'batch_size', 1: 'sequence'},
-            'token_type_ids': {0: 'batch_size', 1: 'sequence'},
             'embedding': {0: 'batch_size'},
         },
     )
 
-    optimized_model = optimizer.optimize_model(
-        './model_repository/embedding/1/v1.onnx',
-        model_type='bert',
-        num_heads=16,
-        hidden_size=1024,
-        opt_level=99,
-        use_gpu=True,
-    )
-    optimized_model.save_model_to_file('./model_repository/embedding/1/v1.onnx')
+    try:
+        optimized_model = optimizer.optimize_model(
+            './model_repository/embedding/1/model.onnx',
+            model_type='bert',
+            num_heads=16,
+            hidden_size=1024,
+            opt_level=99,
+            use_gpu=True,
+        )
+        optimized_model.save_model_to_file('./model_repository/embedding/1/model.onnx')
+    except Exception:
+        print('failed to optimize the onnx model')
 
 
 if __name__ == '__main__':
